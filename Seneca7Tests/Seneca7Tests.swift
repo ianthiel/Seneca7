5164dae25a3e66317064c656e0eefaa69e7cc284
ianthiel <thiel.ian@gmail.com>
2015-12-01 13:44:42 -0800

    Initial Commit

---
+++ Seneca7Tests/Seneca7Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Seneca7Tests.swift
+//  Seneca7Tests
+//
+//  Created by Ian Thiel on 12/1/15.
+//  Copyright © 2015 Ian Thiel. All rights reserved.
+//
+
+import XCTest
+@testable import Seneca7
+
+class Seneca7Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
