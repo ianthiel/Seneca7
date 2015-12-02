9262578b85e023212c461b85b3891fe2d7dcb701
ianthiel <thiel.ian@gmail.com>
2015-12-02 11:56:30 -0800

    Setup location services and map + check if location services are enabled

--- Seneca7/ViewController.swift
+++ Seneca7/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mainMapView: MKMapView!
     
@@ -17,10 +18,38 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var hoursAtOfficeThisWeekLabel: UILabel!
     
+    // setup a location manager as an instance of CLLocationmManager class
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello World")
+        
+        // setup location stuff
+        self.locationManager.delegate = self
+        // we'll need accuracy to be < 10 meters since the work space may be small
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // we'll need "always" authorization to listen for when the user enters/leaves work
+        locationManager.requestAlwaysAuthorization()
+        // we want to begin monitoring location once the user starts the app
+        locationManager.startUpdatingLocation()
+        
+        // setup map stuff
+        mainMapView.delegate = self
+        // we need the standard apple map, nothing fancy
+        mainMapView.mapType = MKMapType.Standard
+        // we want the map to display user location
+        mainMapView.showsUserLocation = true
+        
+        if CLLocationManager.locationServicesEnabled() {
+            switch(CLLocationManager.authorizationStatus()) {
+            case .NotDetermined, .Restricted, .Denied:
+                print("No location access")
+            case .AuthorizedAlways, .AuthorizedWhenInUse:
+                print("Location access")
+            }
+        } else {
+            print("Location services are not enabled")
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- Seneca7Tests/Seneca7Tests.swift
+++ Seneca7Tests/Seneca7Tests.swift
@@ -13,7 +13,6 @@ class Seneca7Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
@@ -21,9 +20,8 @@ class Seneca7Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLocationCaptured() {
+        XCTAssertEqual(1+1,2, "1+1 should equal 2")
     }
     
     func testPerformanceExample() {
