ef29b3753fc03d7f5b5bb7bc12102c0f4d97443d
ianthiel <thiel.ian@gmail.com>
2015-12-06 11:25:32 -0800

    added workLocations count display, add, remove, and update functions + save and load functions

--- Seneca7/ViewController.swift
+++ Seneca7/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 import CoreLocation
 import MapKit
 
+let kSavedItemsKey = "savedItems"
+
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    var workLocations = [WorkLocation]()
+    
     @IBOutlet weak var mainMapView: MKMapView!
     @IBOutlet weak var workLocationsLabel: UILabel!
     @IBOutlet weak var hoursAtOfficeThisWeekLabel: UILabel!
+    @IBOutlet weak var workLocationsCountLabel: UILabel!
+    @IBOutlet weak var workLocationsCountDisplay: UILabel!
+    
     
     // setup a location manager as an instance of CLLocationmManager class
     let locationManager = CLLocationManager()
@@ -43,6 +50,53 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     @IBAction func onZoomToUserLocation(sender: AnyObject) {
         zoomToUserLocationInMapView(mainMapView)
+    }
+    
+    // MARK: Saving and loading functions for workLocations
+    
+    func saveAllWorkLocations() {
+        let items = NSMutableArray()
+        for workLocation in workLocations {
+            let item = NSKeyedArchiver.archivedDataWithRootObject(workLocation)
+            items.addObject(item)
+        }
+        NSUserDefaults.standardUserDefaults().setObject(items, forKey: kSavedItemsKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    func loadAllWorkLocations() {
+        workLocations = []
+        
+        if let savedItems = NSUserDefaults.standardUserDefaults().arrayForKey(kSavedItemsKey) {
+            for savedItem in savedItems {
+                if let workLocation = NSKeyedUnarchiver.unarchiveObjectWithData(savedItem as! NSData) as? WorkLocation {
+                    addWorkLocation(workLocation)
+                }
+            }
+        }
+    }
+    
+    // MARK: add, remove, and update functions for workLocation model changes
+    
+    func addWorkLocation(workLocation: WorkLocation) {
+        workLocations.append(workLocation)
+        mainMapView.addAnnotation(workLocation)
+        // addRadiusOverlayForWorkLocation(workLocation)
+        // updateWorkLocationsCount()
+    }
+    
+    func removeWorkLocation(workLocation: WorkLocation) {
+        if let indexInArray = workLocations.indexOf(workLocation) {
+            workLocations.removeAtIndex(indexInArray)
+        }
+        
+        mainMapView.removeAnnotation(workLocation)
+        // removeRadiusOverlayForWorkLocation(workLocation)
+        // updateWorkLocationsCount()
+    }
+    
+    func updateWorkLocationsCount() {
+        workLocationsCountDisplay.text = "\(workLocations.count)"
     }
     
     func tests() {
