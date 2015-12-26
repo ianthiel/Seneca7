e01b7994037c8896a46c4d235ddad42e4e48bcd9
ianthiel <thiel.ian@gmail.com>
2015-12-26 13:20:55 -0500

    killed unused WorkWeek data model

--- Seneca7/WorkDataViewController.swift
+++ Seneca7/WorkDataViewController.swift
@@ -23,20 +23,19 @@ class WorkDataViewController: UIViewController {
         convertHoursToHoursMinutesAndPrint()
     }
     
-    
     func convertHoursToHoursMinutesAndPrint() {
         let minutesWorkedTotal = userDefaults.valueForKey("minutes") as! Double
         let hoursWorkedTotal = minutesWorkedTotal / 60.0
         let hoursWorked = Int(floor(hoursWorkedTotal))
         let minutesWorked = Int(floor(minutesWorkedTotal % 60.0))
         if hoursWorked == 1 && minutesWorked == 1 {
-            hoursWorkedDisplay.text = "\(hoursWorked) hour and \(minutesWorked) minute at work."
+            hoursWorkedDisplay.text = "All time: \(hoursWorked) hour and \(minutesWorked) minute at work."
         } else if hoursWorked == 1 {
-            hoursWorkedDisplay.text = "\(hoursWorked) hour and \(minutesWorked) minutes at work."
+            hoursWorkedDisplay.text = "All time: \(hoursWorked) hour and \(minutesWorked) minutes at work."
         } else if minutesWorked == 1 {
-            hoursWorkedDisplay.text = "\(hoursWorked) hours and \(minutesWorked) minute at work."
+            hoursWorkedDisplay.text = "All time: \(hoursWorked) hours and \(minutesWorked) minute at work."
         } else {
-            hoursWorkedDisplay.text = "\(hoursWorked) hours and \(minutesWorked) minutes at work."
+            hoursWorkedDisplay.text = "All time: \(hoursWorked) hours and \(minutesWorked) minutes at work."
         }
     }
     
